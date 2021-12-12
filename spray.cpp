923c84e68c2cbbf4283e38e01b65d3fec40fdb51
bariscodefx <bariscodefx@gmail.com>
2021-12-12 08:55:15 +0300

    Create spray.cpp

---
+++ spray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int spgay;
+
+    cout << "SPGAY: ";
+    cin >> spgay;
+
+    if (spgay == 31) {
+        cout << "lol";
+    }
+    else {
+        cout << "aptal";
+    }
+    return 0;
+}
