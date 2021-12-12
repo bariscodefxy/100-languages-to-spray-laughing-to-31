a3210ea9c7bb90ac0bc056b958006efc4c1f61a0
Drum3X <86149093+Drum3X@users.noreply.github.com>
2021-12-12 21:18:15 +0300

    added javascript support
    spray.js

---
+++ spray.js
@@ -0,0 +1,16 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question("SPGAY:\n", spgay => {
+
+if (spgay == "31") {
+  console.log("lol")
+} 
+
+else {
+  console.log("aptal") 
+} 
+  readline.close()
+})  
