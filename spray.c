cd9351cf62f7aab05a57c8c74a9ae768b2ff0cd6
bariscodefx <bariscodefx@gmail.com>
2021-12-12 08:54:02 +0300

    Create spray.c

---
+++ spray.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int spgay;
+
+    printf("SPGAY:");
+    scanf("%d",&spgay);
+    
+    if(spgay == 31)
+    {
+      printf("lol");
+    }
+    else{
+      printf("aptal");
+    }
+    return 0;
+}
