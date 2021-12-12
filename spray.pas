43cbd3de75693d1d24513190dbc1804e463cdcac
bariscodefx <bariscodefx@gmail.com>
2021-12-12 19:30:22 +0300

    Create spray.pas

---
+++ spray.pas
@@ -0,0 +1,15 @@
+program spgay(input, output);
+var
+	response: char;
+begin
+	writeLn('SPGAY: ');
+	
+	readLn(input, response);
+	
+	if response = '31' then
+	begin
+		writeLn('lol');
+  else
+    writeLn('aptal');
+	end;
+end
