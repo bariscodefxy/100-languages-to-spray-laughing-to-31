6bbeea78dae81554771725a069eeb045c451707e
bariscodefx <bariscodefx@gmail.com>
2021-12-12 16:05:01 +0300

    Create spray.go

---
+++ spray.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var spgay string
+	fmt.Println("SPGAY: ")
+	fmt.Scanln(&spgay)
+	
+	if spgay == "31" {
+	    fmt.Println("lol")
+	} else {
+	    fmt.Println("aptal")
+	}
+
+} 
