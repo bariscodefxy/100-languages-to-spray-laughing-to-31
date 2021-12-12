193c665f26348d18084939d002221a5fdb841054
Drum3X <86149093+Drum3X@users.noreply.github.com>
2021-12-12 16:08:16 +0300

    golang (#1)
    * Create spray.go
    
    * Update spray.go

---
+++ spray.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var spgay string
+	fmt.Println("SPGAY: ")
+	fmt.Scanln(&spgay)
+	
+	if spgay == "31" {
+	    fmt.Println("lol")
+	} else {
+	    fmt.Println("aptal")
+	}
+
+} 
+
