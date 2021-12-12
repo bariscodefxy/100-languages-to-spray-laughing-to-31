70782030eaab8e192762df5f4de83b41b8724a23
bariscodefx <bariscodefx@gmail.com>
2021-12-12 09:12:06 +0300

    Create spray.java

---
+++ spray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Spgay {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("SPGAY: "); 
+        int spgay = sc.nextInt();
+        if(spgay == "31") {
+            System.out.println("lol");
+        }else {
+            System.out.println("aptal");
+        }
+    }
+
+}
